0e0f627009b98a06cee0c0739babad409c1f75c6
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-19 18:12:46 -0300

    Add files via upload
    Funções que retornam mais deu um valor!

---
+++ funcoes_que_retornam_mais_de_um_valor.py
@@ -0,0 +1,12 @@
+dados = {
+   'Crossfox': {'km': 35000, 'ano': 2005},
+   'DS5': {'km': 15000, 'ano': 2017},
+   'Corolla': {'km': 180000, 'ano': 2007}
+}
+def km_media(dataset, ano_atual):
+   resultado = {}
+   for itens in dataset.items():
+      operacao = itens[1]['km'] / (ano_atual - itens[1]['ano'])
+      resultado.update({itens[0] : operacao})
+   return resultado
+print(km_media(dados, 2019)) # Resposta: {'Crossfox': 2500.0, 'DS5': 7500.0, 'Corolla': 15000.0}.
