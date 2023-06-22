fd96dcaed8c9016e8c572c008e0528e69b4f522b
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-22 19:35:05 -0300

    Add files via upload

---
+++ selecoes_em_listas-a[i].py
@@ -0,0 +1,6 @@
+carro_1 = ['Jetta Variant', 'Motor 4.0 Turbo', 2003, 44410.0, False, ['Rodas de liga', 'Travas elétricas', 'Piloto automático'], 88078.64] # Lista.
+carro_2 = ['Passat', 'Motor Diesel', 1991, 5712.0, False, ['Central multimídia', 'Teto panorâmico', 'Freios ABS'], 106161.94] # Lista.
+carros = carro_1 + carro_2
+print(carros[0]) # Jetta Variant.
+print(carros[-2]) # ['Central multimídia', 'Teto panorâmico', 'Freios ABS'].
+print(carros[-2][1]) # Teto panorâmico.
