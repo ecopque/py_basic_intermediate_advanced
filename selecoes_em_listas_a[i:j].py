016085a8c10a5f0149ff31a2388becb459a9daa2
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-23 19:17:58 -0300

    Add files via upload

---
+++ selecoes_em_listas_a[i:j].py
@@ -0,0 +1,8 @@
+acessorios = [
+    'Rodas de liga', 'Travas elétricas', 'Piloto automático',
+    'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 
+    'Sensor crepuscular', 'Sensor de chuva'
+    ]
+print(acessorios[2:6]) # ['Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento'].
+print(acessorios[2:]) # ['Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'].
+print(acessorios[:5]) # ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado'].
