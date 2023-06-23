76c008d82dff00191148d3f746ee689bd0298a72
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-23 19:18:57 -0300

    Update formatacao_string_f-string.py

--- formatacao_string_f-string.py
+++ formatacao_string_f-string.py
@@ -1,3 +1,3 @@
 nome2 = 'Edson1'
 acesso2 = 101
-print(f'Olá, {nome2}! Este é seu acesso de número {acesso2}!') # Definitivo.
+print(f'Olá, {nome2}! Este é seu acesso de número {acesso2}!') # Definitivo, ideal.
