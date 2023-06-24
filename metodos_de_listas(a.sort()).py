ec83d1a8c38bb6cba3b9b582baa3600dac773eb7
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-24 17:20:24 -0300

    Add files via upload
    Função sort().

---
+++ metodos_de_listas(a.sort()).py
@@ -0,0 +1,7 @@
+acessorios = [
+    'Rodas de liga', 'Travas elétricas', 'Piloto automático',
+    'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 
+    'Sensor crepuscular', 'Sensor de chuva'
+    ]
+acessorios.sort() # Classifica a lista em ordem alfabética e imprime a lista ordenada.
+print(acessorios)
