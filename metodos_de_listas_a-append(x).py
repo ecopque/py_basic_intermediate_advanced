a77c5d7cc67f99d10772bd5ce1f195b1c58a2869
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-25 16:22:15 -0300

    Add files via upload
    append().

---
+++ metodos_de_listas_a-append(x).py
@@ -0,0 +1,8 @@
+acessorios = [
+    'Rodas de liga', 'Travas elétricas', 'Piloto automático',
+    'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 
+    'Sensor crepuscular', 'Sensor de chuva'
+    ]
+acessorios.append('4 x 4')
+acessorios.sort() # Classifica a lista em ordem alfabética e imprime a lista ordenada.
+print(acessorios)
