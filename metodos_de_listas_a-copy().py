2383f0c766fcafcd6b5e3094652d24dd2bcdb7f6
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-26 17:46:46 -0300

    Add files via upload

---
+++ metodos_de_listas_a-copy().py
@@ -0,0 +1,11 @@
+acessorios = [
+    'Rodas de liga', 'Travas elétricas', 'Piloto automático',
+    'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 
+    'Sensor crepuscular', 'Sensor de chuva'
+    ]
+acessorios.append('4 x 4')
+acessorios.sort()
+acessorios_2 = acessorios.copy() # Cópia rasa!
+acessorios_3 = acessorios_2[:] # Cópia rasa de "acessorios_2".
+print(acessorios_2)
+print(acessorios_3)
