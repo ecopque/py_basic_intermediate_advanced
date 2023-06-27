a1855b8315461f43722a6d4d39d0a123bcdc6422
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-27 19:14:08 -0300

    Add files via upload

---
+++ range()_for.py
@@ -0,0 +1,2 @@
+for i in range(0, 10, 2): # Criar uma sequência de números de 0 a 9, com um passo de 2 (ou seja, pulando de dois em dois números). A cada iteração do loop for, o valor atual da sequência é armazenado na variável "i".
+    print(i) # Resposta 0, 2, 4, 6, 8.
