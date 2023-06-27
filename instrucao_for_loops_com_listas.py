3ed1e974b701a891babf94a7adf62db95b9f98a1
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-27 19:12:02 -0300

    Add files via upload

---
+++ instrucao_for_loops_com_listas.py
@@ -0,0 +1,3 @@
+acessorios = ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'] # Lista.
+for itens in acessorios: # Loop "for" para percorrer cada item na lista e imprimi-lo na tela. O resultado final será a impressão de cada item da lista em uma nova linha.
+   print(itens)
