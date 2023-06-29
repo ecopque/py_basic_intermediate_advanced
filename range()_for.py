59eb0198f387f139ad0d25b1306cca7bb2b0e629
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-28 23:08:41 -0300

    Update range()_for.py
    Dois print().

--- range()_for.py
+++ range()_for.py
@@ -1,3 +1,4 @@
 for i in range(0, 10, 2): # Criar uma sequência de números de 0 a 9, com um passo de 2 (ou seja, pulando de dois em dois números). A cada iteração do loop for, o valor atual da sequência é armazenado na variável "i".
     print(i) # Resposta 0, 2, 4, 6, 8.
 print(list(range(10))) # Cria um objeto range em forma de lista. Resposta [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].
+print(i ** 2) # Em cada iteração, o número é elevado ao quadrado. Resposta 0, 1, 4, 9, 16, 25, 36, 49, 64, 81.
