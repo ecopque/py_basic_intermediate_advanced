095c19bc0ec4a0af4e3c3dc900c30d1a7f8cdb8a
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-29 20:52:57 -0300

    Update range()_for.py

--- range()_for.py
+++ range()_for.py
@@ -7,3 +7,5 @@ quadrado = []
 for i in range(10):
    quadrado.append(i ** 2)
 print(quadrado)
+##########################################################
+[i ** 2 for i in range(10)] # List Comprehension. Não gosto desta forma. Deselegante!
