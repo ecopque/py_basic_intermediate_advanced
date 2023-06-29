527526026b1cdd385ecc8ef7bc4b9417593925c2
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-06-28 23:07:51 -0300

    Update range()_for.py

--- range()_for.py
+++ range()_for.py
@@ -1,2 +1,3 @@
 for i in range(0, 10, 2): # Criar uma sequência de números de 0 a 9, com um passo de 2 (ou seja, pulando de dois em dois números). A cada iteração do loop for, o valor atual da sequência é armazenado na variável "i".
     print(i) # Resposta 0, 2, 4, 6, 8.
+print(list(range(10))) # Cria um objeto range em forma de lista. Resposta [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].
