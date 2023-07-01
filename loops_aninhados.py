7930ce362a6e105abc71ae8ae5c26d8108975065
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-07-01 18:51:56 -0300

    Add files via upload

---
+++ loops_aninhados.py
@@ -0,0 +1,7 @@
+dados = [ 
+    ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'],
+    ['Central multimídia', 'Teto panorâmico', 'Freios ABS', '4 X 4', 'Painel digital', 'Piloto automático', 'Bancos de couro', 'Câmera de estacionamento'],
+    ['Piloto automático', 'Controle de estabilidade', 'Sensor crepuscular', 'Freios ABS', 'Câmbio automático', 'Bancos de couro', 'Central multimídia', 'Vidros elétricos']
+]
+for lista in dados:
+   print(lista)
