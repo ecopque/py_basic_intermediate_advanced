7c3cf0f1d698353fde1b8d362e3b90e92a22289d
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-07-14 21:53:27 -0300

    Update instrucao_if_operadores_de_comparacao_e_operadores_logicos_and_or_not.py

--- instrucao_if_operadores_de_comparacao_e_operadores_logicos_and_or_not.py
+++ instrucao_if_operadores_de_comparacao_e_operadores_logicos_and_or_not.py
@@ -19,3 +19,9 @@ for lista in dados: # Itera sobre a lista "dados" usando um loop for, onde em ca
    if(lista[2] == True): # Verifica se o valor do terceiro elemento da sublista é igual a True usando uma condicional if.
       zero_km_y.append(lista) # Se a condição for verdadeira, a sublista é adicionada à lista "zero_km_y" usando o método "append".
 print(zero_km_y) # Resposta: [['DS5', 2019, True], ['A5', 2019, True]].
+###############################################
+zero_km_n = [] # Define uma lista vazia.
+for lista in dados: # Itera sobre a lista "dados", atribuindo cada sublista a "lista".
+   if(lista[2] == False): # Verifica se o terceiro elemento de lista é "False".
+      zero_km_n.append(lista) # Adiciona a sublista atual (lista) à lista "zero_km_n" se o terceiro elemento for False.
+print(zero_km_n) # Resposta: [['Jetta Variant', 2003, False], ['Passat', 1991, False], ['Crossfox', 1990, False], ['Aston Martin DB4', 2006, False], ['Palio Weekend', 2012, False], ['Série 3 Cabrio', 2009, False], ['Dodge Jorney', 2019, False], ['Carens', 2011, False]].
