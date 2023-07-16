9744db0f458d00a99126cefc9c5e2fb8d9482fb4
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-07-15 21:30:58 -0300

    Update listas_simples.py
    Simple!

--- listas_simples.py
+++ listas_simples.py
@@ -1,4 +1,4 @@
 # Listas simples.
 
 acessorios = ['Rodas de liga', 'Travas Elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'] # Lista.
-print(type(acessorios)) # Retorna o tipo do objeto em questão. <class 'list'>.
+print(type(acessorios)) # Retorna o tipo do objeto em questão. <class 'list'>!
