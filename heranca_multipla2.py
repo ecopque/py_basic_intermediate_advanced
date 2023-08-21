e5e9654b95a86d87a14f51de7eaa525c61500efa
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-08-21 16:12:45 -0300

    Add files via upload

---
+++ heranca_multipla2.py
@@ -0,0 +1,23 @@
+class A:
+    ...
+    def quem_sou(self):
+        print('A')
+class B(A):
+    ...
+    # def quem_sou(self):
+    #     print('B')
+class C(A):
+    ...
+    def quem_sou(self):
+        print('C')
+class D(B, C):
+    ...
+    # def quem_sou(self):
+    #     print('D')
+d = D()
+d.quem_sou()
+
+
+
+print(D.__mro__) #1:
+print(D.mro()) #2:
