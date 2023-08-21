f658466caa8911da6d2436e14ac55b36d92fcb62
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-08-21 11:11:05 -0300

    Add files via upload

---
+++ heranca_multipla.py
@@ -0,0 +1,13 @@
+class Pai1:
+    def metodo_pai1(self):
+        print("Método do Pai 1")
+class Pai2:
+    def metodo_pai2(self):
+        print("Método do Pai 2")
+class Filha(Pai1, Pai2):
+    def metodo_filha(self):
+        print("Método da Filha")
+obj = Filha() #1:
+obj.metodo_filha() #2:
+obj.metodo_pai1() #3:
+obj.metodo_pai2()
