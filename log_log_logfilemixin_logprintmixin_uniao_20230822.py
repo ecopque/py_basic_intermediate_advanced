edacf4328a5812951bc8f50f46d669e4856edc15
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-08-22 14:47:55 -0300

    LogFileMixin e LogPrintMixin
    Utilização do LogFileMixin e LogPrintMixin, utilizando log.py e main.py.

---
+++ log_log_logfilemixin_logprintmixin_uniao_20230822.py
@@ -0,0 +1,20 @@
+class Log:
+    def _log(self, msg):
+        raise NotImplementedError('Implemente o método log.')
+    def log_erro(self, msg):
+        return self._log(f'Erro: {msg}')
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+    
+class LogFileMixin(Log):
+    def _log(self, msg):
+        print(msg)
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+if __name__ == '__main__':
+    l = LogPrintMixin()
+    l.log_erro('Usando log_erro!')
+    l.log_success('Usando log_success!')
---
+++ main_log_logfilemixin_logprintmixin_uniao_20230822.py
@@ -0,0 +1,3 @@
+from log import Log
+l = Log()
+l.log('Dentro de main.py')
