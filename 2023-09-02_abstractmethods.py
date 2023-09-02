d1948b5c9f3f7cda2c3f6085b3b999dfc8f5381c
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-02 15:58:45 -0300

    Add files via upload

---
+++ 2023-09-02_abstractmethods.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+class AbstractFoo(ABC):
+    def __init__(self, name):
+        self._name = None
+        self.name = name
+    @property
+    @abstractmethod #1: 
+    def name(self): ...
+class Foo(AbstractFoo):
+    def __init__(self, name):
+        super().__init__(name)
+        print('Sou inútil')
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        self._name = name
+foo = Foo('Bar')
+print(foo.name)
