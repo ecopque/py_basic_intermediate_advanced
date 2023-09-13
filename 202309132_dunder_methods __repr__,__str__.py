356af9f65d338324d4cad46925bcaf1707d9209d
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-13 15:39:32 -0300

    Add files via upload

---
+++ 202309132_dunder_methods __repr__,__str__.py
@@ -0,0 +1,17 @@
+class Ponto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return f'({self.x}, {self.y})'    
+    def __repr__(self):
+        class_name = self.__class__.__name__
+        class_name2 = type(self).__name__
+        return f'{class_name}(x={self.x}, y={self.y})'
+p1 = Ponto(3, 6)
+p2 = Ponto(123, 789)
+print(p1) #1: 
+print(p2) #2:
+
+#1: Resposta: (3, 6).
+#2: Resposta: (123, 789).
