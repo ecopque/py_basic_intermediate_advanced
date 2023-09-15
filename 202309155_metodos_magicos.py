dd4608e92af32d883c3f04e04bb712fb29b92561
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-15 16:57:55 -0300

    Add files via upload

---
+++ 202309155_metodos_magicos.py
@@ -0,0 +1,9 @@
+class Pessoa:
+    def __init__(self):
+        self.pessoas = ['Edson', 'Neto', 'Théo', 'Mel']
+    def __reversed__(self):
+        return [self.pessoas[i] for i in range(len(self.pessoas) -1, -1, -1)]
+x = Pessoa()
+print(reversed(x)) #1:
+
+#1: Resposta: ['Mel', 'Théo', 'Neto', 'Edson'].
