badbacbfb5c97ef76d93cef2ccb7518f480aa477
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-15 09:33:08 -0300

    Add files via upload

---
+++ 202309151_metodos_magicos.py
@@ -0,0 +1,19 @@
+class Pessoa:
+    def __init__(self):
+        self.pessoas = ['Edson', 'Neto', 'Théo', 'Mel']
+    def __len__(self) -> int:
+        x = 0 #1:
+        while True: #2:
+            try:
+                y = self.pessoas[x]
+                x += 1 #3:
+            except:
+                break
+        return x
+x = Pessoa()
+print(len(x)) #4:
+
+#1: Contador.
+#2: Enquanto for verdade (loop infinito).
+#3: A cada iteração de y, x recebe mais um.
+#4: Resposta: 4.
