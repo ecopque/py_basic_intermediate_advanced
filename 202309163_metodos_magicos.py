c86491f855e357694bdec86a7353ed822a42ba59
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-16 20:49:48 -0300

    Add files via upload

---
+++ 202309163_metodos_magicos.py
@@ -0,0 +1,24 @@
+class Pessoa:
+    def __init__(self):
+        self.pessoas = ['Edson', 'Neto', 'Théo', 'Mel']
+    def __len__(self) -> int:
+        x = 0
+        while True:
+            try:
+                y = self.pessoas[x]
+                x += 1
+            except:
+                break
+        return x
+    def __getitem__(self, posicao):
+        return self.pessoas[posicao]
+    def __iter__(self):
+        i = 0
+        while i < len(self.pessoas):
+            yield self.pessoas[i]
+            i += 1
+minha_pessoa = Pessoa()
+for i in minha_pessoa:
+    print(i) #1:
+
+#1: Resposta: Edson, Neto, Théo, Mel.
