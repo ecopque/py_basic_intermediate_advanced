70ddb6d3ec25574b13b9bb9774884c465f5d5ee9
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-17 20:09:43 -0300

    Add files via upload

---
+++ 202309172_exemplos_dunder_methods.py
@@ -0,0 +1,29 @@
+class Ponto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f'{class_name} (x={self.x!r}, y={self.y!r})'
+    
+    def __add__(self, other):
+        novo_x = self.x + other.x
+        novo_y = self.y + other.y
+        return Ponto(novo_x, novo_y)
+
+    def __gt__(self, other):
+        resultado_self = self.x + self.y
+        resultado_other = other.x + other.y
+        return resultado_self > resultado_other
+    
+if __name__ == '__main__':
+    p1 = Ponto(4, 2) #1:
+    p2 = Ponto(6, 4) #2:
+    p3 = p1 + p2
+    print(p3)
+    print('P1 é maior que P2? Resposta:', p1 > p2) #3:
+
+#1: A soma de 4 e 2 = 6.
+#2: A soma de 6 e 4 = 10.
+#3: Resposta: False.
