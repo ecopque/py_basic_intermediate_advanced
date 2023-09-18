310b570e4b8c7c09bfded6173ef3faa1488d73f6
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-18 10:28:29 -0300

    Add files via upload

---
+++ 202309181_metodo__new__.py
@@ -0,0 +1,14 @@
+class A(): #1:
+    def __new__(cls):
+        return super().__new__(cls) #2:
+    
+    def __init__(self):
+        print('Olá, __init__')
+
+    def __repr__(self):
+        return 'Vsf!'
+    
+a = A()
+
+#1: Poderia fazer "class A(object):", que daria no mesmo.
+#2: Em outro momento foi feito: "return object.__new__(A)" e obtivemos o mesmo resultado.
