cc9ab69e0699370562f847735a6102b009d2143b
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-19 10:24:11 -0300

    Add files via upload
    Atualizando!

--- 202309191_context_manager.py
+++ 202309191_context_manager.py
@@ -1,18 +1,18 @@
 class MyContextManager():
-
-    def __init__(self):
+    def __init__(self, caminho_arquivo, modo):
         print('Init')
-
+        self.caminho_arquivo = caminho_arquivo
+        self.modo = modo
+    
     def __enter__(self):
-        print('Enter')
-        return 123
-
+        print('Enter: abrindo arquivo')
+        return open(self.caminho_arquivo, self.modo, encoding='utf8') #1:
+    
     def __exit__(self, class_exception, exception_, traceback_):
-        print('Exit')
+        print('Exit: fechando arquivo')
 
-instancia = MyContextManager()
+instancia = MyContextManager('app.py', 'w')
+with instancia as arquivo:
+    print('With', arquivo)
 
-with instancia as alguma_coisa:
-    print('With', alguma_coisa) #1:
-
-#1: Resposta: Init | Enter | With 123 | Exit.
+#1: Quando for abrir um arquivo sem "with", devemos fechá-lo.
