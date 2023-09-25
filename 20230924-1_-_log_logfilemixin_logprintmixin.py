072c5fea1db9a03d96eb3d4e41d8c635a07bc2d9
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-24 21:29:04 -0300

    Add files via upload

---
+++ 20230924-1_-_log_logfilemixin_logprintmixin.py
@@ -0,0 +1,17 @@
+class Log: #1: #2:
+    def _log(self, msg): #3:
+        raise NotImplementedError('Implemente o método log.')
+    def log_error(self, msg):
+        return self._log(f'Erro: {msg}')
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+class LogFileMixin(Log):
+    def _log(self, msg):
+        print(msg)
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+if __name__ == '__main__':
+    l = LogPrintMixin()
+    l.log_error('Qualquer coisa...')
+    l.log_success('Que coisa linda...')
