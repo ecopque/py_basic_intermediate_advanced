f1e099c789a1e173e93b6715c9152b9e90fb463d
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-26 10:22:59 -0300

    Add files via upload

---
+++ 2023-09-26-2_-_dir()_e_help()_+_docstring_de_uma_linha.py
@@ -0,0 +1,7 @@
+import temp
+
+help(temp) #1: #2:
+
+#1: Como resposta no terminal, teremos informações sobre: NAME, DATA, FILE, FUNCTIONS (quando houver) e continuamos como se estivéssemos no VI. rss
+#2: Verifique que agora que utilizamos uma DocString no temp.py, obtivemos no NAME a seguinte saída: temp - Ei amigo, estamos dentro do temp.py usando uma DocString!).
+#3: Obs: Usando help() em noss main, com uma importação de um arquivo *.py que contenha uma DocString, obteremos no campo NAME a frase ao lado do nome do arquivo importado.
