7db84d928d397aa6b629698f8e1161033819263b
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-09-26 15:11:24 -0300

    Add files via upload

---
+++ 2023-09-26-1_-_docstrings_de_varias_linhas(documentacao).py
@@ -0,0 +1,18 @@
+import temp
+
+help(temp)
+
+#Obs: Máximo 80/linha.
+#Obs2: O título deve estar rente às aspas. Ex: "Título bla bla (...). Abaixo, texto será incluído na descrição.
+#Obs3: O texto "description" deve estar uma quebra de linha abaixo do título.
+
+
+# Abaixo, conteúdo do módulo temp.py.
+
+# """ Fazendo uso de textos longos 
+#
+# The Voyager Golden Records are two identical phonograph records one of each which were included aboard the two Voyager spacecraft launched in 1977.[1] The records contain sounds and images selected to portray the diversity of life and culture on Earth, and are intended for any intelligent extraterrestrial life form who may find them. The records are a time capsule. 
+# """
+# import math
+# operacoes = math.sqrt(25)
+# print(operacoes)
