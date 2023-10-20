e2fc3019fa200c37ee191d60e72338534b6a5932
Edson C.Santos <105827582+ecopsantos1@users.noreply.github.com>
2023-10-20 16:21:24 -0300

    Add files via upload

---
+++ 2023-10-20_-_modulo_os_para_interacao_com_o_sistema.py
@@ -0,0 +1,10 @@
+import os
+
+os.system('clear') #1:
+os.system('echo "Hello World"') #2: #3:
+print('a' * 10)
+
+#1: Limpando tela do terminal antes do print.
+#2: Não foi o Python que fez. Apenas orientou ao sistema.
+#3: Resposta: Hello World.
+#4: Resposta:aaaaaaaaaa.
