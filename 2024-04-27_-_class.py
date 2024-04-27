56ac4910e3b2e15e9880b6801f2d1fa9d71f9f56
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-27 20:05:54 -0300

    Create 2024-04-27_-_class.py

---
+++ 2024-04-27_-_class.py
@@ -0,0 +1,10 @@
+class Pessoa:
+    def __init__(self, nome, sobrenome): #1: #2:
+        self.nome = nome #3:
+        self.sobrenome = sobrenome
+p1 = Pessoa('Edson', 'Santos') #4:
+p2 = Pessoa('Théo', 'Mel')
+print(p1.nome) #5:
+print(p1.sobrenome) #6:
+print(p2.nome) #7:
+print(p2.sobrenome) #8:
