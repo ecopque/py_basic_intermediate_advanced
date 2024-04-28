8d7ad5bef34332ea4270e3cb8f355a5f3a47b07c
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-28 19:51:30 -0300

    Create 2024-04-28_-_heranca.py

---
+++ 2024-04-28_-_heranca.py
@@ -0,0 +1,11 @@
+class Pessoa:
+    def exibe_cpf(self):
+        return '123456789'
+class Secretaria(Pessoa): #1:
+    pass
+class Vendedor(Pessoa): #2:
+    pass
+s1 = Secretaria()
+v1 = Vendedor()
+print(s1.exibe_cpf()) #3:
+print(v1.exibe_cpf()) #4:
