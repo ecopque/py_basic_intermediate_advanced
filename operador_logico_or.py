7ee45e678d06466668416dfaff49f7cf86ccd48a
Edson Copque <105827582+ecopque@users.noreply.github.com>
2024-04-29 19:55:36 -0300

    Create operador_logico_or.py

---
+++ operador_logico_or.py
@@ -0,0 +1,7 @@
+login = input('[E]ntrar ou [S]air: ')
+senha = input('Senha: ')
+senha_permitida = '123456'
+if (login == 'E' or login == 'e') and senha == senha_permitida:
+    print('Você está logado.')
+else:
+    print('Você saiu.')
