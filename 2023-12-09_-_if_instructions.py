6faa7602c962db2b5600471c3f6be02dd9578761
Edson Copque <ecop@disroot.org>
2024-11-08 16:32:49 -0300

    Create 2023-12-09_-_if_instructions.py

---
+++ 2023-12-09_-_if_instructions.py
@@ -0,0 +1,14 @@
+dados = [
+    ['Jetta Variant', 2003, False],
+    ['Passat', 1991, False],
+    ['Crossfox', 1990, False],
+    ['DS5', 2019, True],
+    ['Aston Martin DB4', 2006, False],
+    ['Palio Weekend', 2012, False],
+    ['A5', 2019, True],
+    ['Série 3 Cabrio', 2009, False],
+    ['Dodge Jorney', 2019, False],
+    ['Carens', 2011, False]
+] #1: 
+for lista in dados:
+   print(lista[2]) #2: #3:
