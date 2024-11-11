f7eb998aeabca30cb316c930e2d3d941ae41cbb0
Edson Copque <ecop@disroot.org>
2024-11-11 17:57:47 -0300

    Create 2024-02-07_-_composicao.py

---
+++ 2024-02-07_-_composicao.py
@@ -0,0 +1,23 @@
+class Cliente:
+    def __init__(self, nome):
+        self.nome = nome
+        self.enderecos = []
+    def inserir_enderecos(self, rua, numero):
+        self.enderecos.append(Endereco(rua, numero))
+    def listar_enderecos(self):
+        for endereco in self.enderecos:
+            print(endereco.rua, endereco.numero)
+    def __del__(self):
+        print('Apagando »', self.nome)
+class Endereco:
+    def __init__(self, rua, numero):
+        self.rua = rua
+        self.numero = numero
+    def __del__(self):
+        print('Apagando »', self.rua, self.numero)
+cliente1 = Cliente('Maria')
+cliente1.inserir_enderecos('Rua 01', 10)
+cliente1.inserir_enderecos('Rua 02', 15)
+cliente1.listar_enderecos() #1:
+del cliente1 #2:
+print('*** Aqui termina meu código...') #3:
