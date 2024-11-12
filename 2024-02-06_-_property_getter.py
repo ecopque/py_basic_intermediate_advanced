d8e30e80203f9b35cb8960ddd5968fb550076789
Edson Copque <ecop@disroot.org>
2024-11-12 19:07:30 -0300

    Create 2024-02-06_-_property_getter.py

---
+++ 2024-02-06_-_property_getter.py
@@ -0,0 +1,14 @@
+class Caneta:
+    def __init__(self, cor):
+        self._cor = cor #1:
+    @property
+    def cor(self): #2:
+        print('Iniciando property...')
+        return self._cor
+    @cor.setter #3:
+    def cor(self, valor):
+        print('Estou no setter: ', valor)
+        self._cor = valor
+canetinha = Caneta(cor='Azul') #4:
+canetinha.cor = 'Vermelha' #5:
+print(canetinha.cor) #6:
