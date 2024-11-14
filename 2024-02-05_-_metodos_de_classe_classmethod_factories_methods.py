5c2d42de4876e5a97e385d3771f546c3c3bc056d
Edson Copque <ecop@disroot.org>
2024-11-13 21:07:35 -0300

    Create 2024-02-05_-_metodos_de_classe_classmethod_factories_methods.py

---
+++ 2024-02-05_-_metodos_de_classe_classmethod_factories_methods.py
@@ -0,0 +1,13 @@
+class Pessoa:
+    ano = 2023 #1:
+    def __init__(self, nome, idade):
+        self.nomeacao = nome
+        self.age = idade
+    @classmethod
+    def criar_com_50_anos(cls, nome):
+        return cls(nome, 50) #3:
+p1 = Pessoa(nome='Edsuuu', idade=100)
+print(Pessoa.ano) #2:
+p2 = Pessoa.criar_com_50_anos('Neto')
+print(p2.nomeacao, p2.age) #4:
+print(p2) #5:
