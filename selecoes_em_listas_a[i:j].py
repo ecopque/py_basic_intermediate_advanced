2c12aae1bd1252967b2d0452bd4cada11d3a3cc2
Edson Copque <ecop@disroot.org>
2024-11-20 09:19:34 -0300

    Update selecoes_em_listas_a[i:j].py

--- selecoes_em_listas_a[i:j].py
+++ selecoes_em_listas_a[i:j].py
@@ -6,3 +6,5 @@ acessorios = [
 print(acessorios[2:6]) # ['Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento'].
 print(acessorios[2:]) # ['Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'].
 print(acessorios[:5]) # ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado'].
+
+# https://linktr.ee/edsoncopque
