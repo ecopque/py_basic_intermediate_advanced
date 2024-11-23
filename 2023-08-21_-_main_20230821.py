22d9ee22e61086f2312e9cac63a4a8f2bd923cd9
Edson Copque <ecop@disroot.org>
2024-11-23 10:50:35 -0300

    Update and rename main_20230821.py to 2023-08-21_-_main_20230821.py

---
+++ 2023-08-21_-_main_20230821.py
@@ -0,0 +1,5 @@
+from log import Log
+l = Log()
+l.log('Dentro de main.py')
+
+# Edson Copque | https://linktr.ee/edsoncopque
--- main_20230821.py
+++
@@ -1,3 +0,0 @@
-from log import Log
-l = Log()
-l.log('Dentro de main.py')
