2f0df21e2d87b6f79d25c12ab93065f3b96459d5
Edson Copque <ecop@disroot.org>
2024-11-25 09:06:33 -0300

    Update and rename instrucao_for_loops_com_listas.py to 2023-06-27_-_instrucao_for_loops_com_listas.py

--- 2023-06-27_-_instrucao_for_loops_com_listas.py
+++ 2023-06-27_-_instrucao_for_loops_com_listas.py
@@ -1,3 +1,5 @@
 acessorios = ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'] # Lista.
 for itens in acessorios: # Loop "for" para percorrer cada item na lista e imprimi-lo na tela. O resultado final será a impressão de cada item da lista em uma nova linha.
    print(itens)
+
+# Edson Copque | https://linktr.ee/edsoncopque
