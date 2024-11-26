7a35e4b96ac4cd6dcb45c80ae0b661429d1da5e1
Edson Copque <ecop@disroot.org>
2024-11-26 09:16:39 -0300

    Update and rename formatacao_string_str-format.py to 2023-06-21_-_formatacao_string_str-format.py

---
+++ 2023-06-21_-_formatacao_string_str-format.py
@@ -0,0 +1,7 @@
+print('Olá, {}!'.format('Edson'))
+
+print('Olá, {}! Esse é seu acesso número {}!'.format('Edson', 100))
+
+print('Olá, {nome}! Este é seu acesso de número {acesso}!'.format(nome = 'Edson', acesso = 100)) # Elegante.
+
+# Edson Copque | https://linktr.ee/edsoncopque
--- formatacao_string_str-format.py
+++
@@ -1,5 +0,0 @@
-print('Olá, {}!'.format('Edson'))
-
-print('Olá, {}! Esse é seu acesso número {}!'.format('Edson', 100))
-
-print('Olá, {nome}! Este é seu acesso de número {acesso}!'.format(nome = 'Edson', acesso = 100)) # Elegante.
