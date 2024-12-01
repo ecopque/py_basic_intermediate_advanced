c03974f7ad4baf5271614d883cf5ebe4979ad018
Edson Copque <ecop@disroot.org>
2024-12-01 09:30:25 -0300

    Update and rename 202309144_metodos_magicos.py to 2023-09-14-4_metodos_magicos.py

--- 2023-09-14-4_metodos_magicos.py
+++ 2023-09-14-4_metodos_magicos.py
@@ -8,3 +8,5 @@ print(x[1]) #2:
 
 #1: __getitem__ é um método especial (ou método dunder, que significa "double underscore" ou "método com duplo sublinhado") que permite que objetos de uma classe suportem a indexação. Isso significa que você pode usar a notação de colchetes [] para acessar elementos de um objeto da classe como se fosse uma sequência (como uma lista ou uma string).
 #2: Resposta: Théo.
+
+# Edson Copque | https://linktr.ee/edsoncopque
