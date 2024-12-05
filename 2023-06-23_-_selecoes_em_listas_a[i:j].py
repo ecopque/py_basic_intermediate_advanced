e45bdf59e5bb32e719dbf4b258516fde72542a93
Edson Copque <ecop@disroot.org>
2024-12-05 09:48:04 -0300

    Update 2023-06-23_-_selecoes_em_listas_a[i:j].py

--- 2023-06-23_-_selecoes_em_listas_a[i:j].py
+++ 2023-06-23_-_selecoes_em_listas_a[i:j].py
@@ -7,4 +7,4 @@ print(acessorios[2:6]) # ['Piloto automático', 'Bancos de couro', 'Ar condicion
 print(acessorios[2:]) # ['Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva'].
 print(acessorios[:5]) # ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado'].
 
-# https://linktr.ee/edsoncopque
+# Edson Copque | https://linktr.ee/edsoncopque
